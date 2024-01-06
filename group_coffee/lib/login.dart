6bd38d8480000ba9d2ced56035c98441095b7a98
bawantha395 <Bawantharathnayake395@gmail.com>
2024-01-06 08:01:42 +0530

    signup

--- group_coffee/lib/login.dart
+++ group_coffee/lib/login.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:group_coffee/forgot.dart';
+import 'package:group_coffee/signup.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -33,7 +36,12 @@ class _LoginState extends State<Login> {
             controller: password,
             decoration: InputDecoration(hintText: 'Enter password'),
           ),
-          ElevatedButton(onPressed: (()=>signin()), child: Text("Login"))
+          ElevatedButton(onPressed: (()=>signin()), child: Text("Login")),
+          SizedBox(height: 30,),
+          ElevatedButton(onPressed: (()=>Get.to(Signup())), child: Text("Register now")),
+          SizedBox(height: 30,),
+          ElevatedButton(onPressed: (()=>Get.to(Forgot())), child: Text("Forgot password ?"))
+          
         ],
       ),
     );
